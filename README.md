aceca10ad4821fe97878578de2886bb3b9090469
lihbr <lihbr@users.noreply.github.com>
2021-06-07 13:47:28 +0200

    chore: init project

---
+++ README.md
@@ -0,0 +1,35 @@
+# dico-example-vite
+
+[![Conventional Commits][conventional-commits-src]][conventional-commits-href]
+
+[Dico.app][dico] example with Vite.
+
+- 🔑 &nbsp;Use keys seamlessly;
+- 🎏 &nbsp;Fetch your current dico.
+- 🌐 &nbsp;i18n support.
+
+## Install
+
+<!-- TODO: Update -->
+
+## Usage
+
+<!-- TODO: Update -->
+
+## Documentation
+
+For full documentation, visit the [official Dico.app documentation][dico-docs].
+
+## License
+
+[MIT License](./LICENSE)
+
+<!-- Links -->
+
+[dico]: https://dico.app
+[dico-docs]: https://docs.dico.io/vite
+
+<!-- Badges -->
+
+[conventional-commits-src]: https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg
+[conventional-commits-href]: https://conventionalcommits.org
---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+
+<button @click="$dicoI18n.setLocale($dicoI18n.locales['en-us'])">en-us</button>
+<button @click="$dicoI18n.setLocale($dicoI18n.locales['fr-fr'])">fr-fr</button>
+{{ $dicoI18n.currentLocal }}
+
+<hr />
+
+<form>
+  login
+
+  <label>
+    {{ $dico.form.login.usernameLabel }}
+    <input type="text" :placeholder="$dico.form.login.usernamePlaceholder" />
+  </label>
+
+  <label>
+    {{ $dico.form.login.passwordLabel }}
+    <input type="text" :placeholder="$dico.form.login.passwordPlaceholder" />
+  </label>
+
+  <small>{{$dico.errors.login.usernameNotFound}}</small>
+  <small>{{$dico.errors.login.passwordHelper}}</small>
+  <small>{{$dico.errors.login.wrongPassword}}</small>
+</form>
+
+<hr />
+
+<form @click="alternateLocale">
+  signup
+
+  <label>
+    {{ $dico.form.signup.usernameLabel }}
+    <input type="text" :placeholder="$dico.form.signup.usernamePlaceholder" />
+  </label>
+
+  <label>
+    {{ $dico.form.signup.passwordLabel }}
+    <input type="text" :placeholder="$dico.form.signup.passwordPlaceholder" />
+  </label>
+
+  <small>{{$dico.errors.signup.usernameNotFound}}</small>
+  <small>{{$dico.errors.signup.passwordHelper}}</small>
+  <small>{{$dico.errors.signup.wrongPassword}}</small>
+</form>
+
+</template>
+
+<script>
+import { useDico } from "./useDico";
+
+export default {
+  setup() {
+    const { $dico, $dicoI18n } = useDico();
+    
+    return {
+      $dico,
+      $dicoI18n
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+label, input, small {
+  display: block
+}
+</style>
