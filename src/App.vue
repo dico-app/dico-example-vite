a6e6807486fb4f4380add63380b097019e39d5a9
lihbr <lihbr@users.noreply.github.com>
2021-06-22 10:00:29 +0200

    feat: reset to start

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,89 @@
 <template>
+<section>
+  <form method="POST" action="#">
 
-<button @click="$dicoI18n.setLocale($dicoI18n.locales['en-us'])">en-us</button>
-<button @click="$dicoI18n.setLocale($dicoI18n.locales['fr-fr'])">fr-fr</button>
-{{ $dicoI18n.currentLocal }}
+    <h2>Login</h2>
 
-<hr />
+    <label>
+      {{ $dico.form.login.usernameLabel }}
+      <input type="text" :placeholder="$dico.form.login.usernamePlaceholder" />
+    </label>
 
-<form>
-  login
+    <label>
+      {{ $dico.form.login.passwordLabel }}
+      <input type="password" :placeholder="$dico.form.login.passwordPlaceholder" />
+    </label>
 
-  <label>
-    {{ $dico.form.login.usernameLabel }}
-    <input type="text" :placeholder="$dico.form.login.usernamePlaceholder" />
-  </label>
+    <button type="submit">{{ $dico.form.login.submitLabel }}</button>
 
-  <label>
-    {{ $dico.form.login.passwordLabel }}
-    <input type="text" :placeholder="$dico.form.login.passwordPlaceholder" />
-  </label>
-
-  <small>{{$dico.errors.login.usernameNotFound}}</small>
-  <small>{{$dico.errors.login.passwordHelper}}</small>
-  <small>{{$dico.errors.login.wrongPassword}}</small>
-</form>
-
-<hr />
-
-<form @click="alternateLocale">
-  signup
-
-  <label>
-    {{ $dico.form.signup.usernameLabel }}
-    <input type="text" :placeholder="$dico.form.signup.usernamePlaceholder" />
-  </label>
-
-  <label>
-    {{ $dico.form.signup.passwordLabel }}
-    <input type="text" :placeholder="$dico.form.signup.passwordPlaceholder" />
-  </label>
-
-  <small>{{$dico.errors.signup.usernameNotFound}}</small>
-  <small>{{$dico.errors.signup.passwordHelper}}</small>
-  <small>{{$dico.errors.signup.wrongPassword}}</small>
-</form>
+  </form>
+</section>
 
 </template>
 
 <script>
-import { useDico } from "./useDico";
-
-export default {
-  setup() {
-    const { $dico, $dicoI18n } = useDico();
-    
-    return {
-      $dico,
-      $dicoI18n
-    }
-  }
-}
+export default {}
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  
+  font-size: 16px;
+
+  margin: 0;
+  padding: 0;
 }
 
-label, input, small {
-  display: block
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+section {
+  width: 100%;
+  max-width: 460px;
+  padding: 2rem;
+}
+
+form {
+  border-radius: 1rem;
+  background: rgb(238, 188, 96);
+  padding: 1rem;
+}
+
+label, input, button {
+  display: block;
+  color: #2c3e50;
+  width: 100%;
+}
+
+label {
+  margin-bottom: 1rem;
+}
+
+input, button {
+  background: #fefefe;
+  border: 2px solid rgb(206, 163, 83);
+  border-radius: 1rem;
+  padding: 1rem .5rem;
+}
+
+input:focus, button:focus {
+  outline: none;
+  border-color: #2c3e50
+}
+
+input {
+  width: 100%;
 }
 </style>
